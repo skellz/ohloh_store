137365da5b1f9ba51876b5b06950147c99add22c
Jay Byoun <jay@jaybyoun.com>
2014-01-07 15:05:38 -0500

    ohloh OAuth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = 'b2b759248af68d5915d7d09ea9b2eecd0c94af503f8858faccd53dbcd8733dd2cfd8c47c9024710bcffb935e1b93c3142a2c13e9fd5cae1c10fe230042fc3a67'
